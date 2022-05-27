a7cdff6c581f0bf6d098aaa28df46344093f4950
bgswaroop <g.s.bennabhaktula@rug.nl>
2022-05-27 16:22:30 +0200

    added paths to dataset_dir and logs_dir for CI tests

--- tests/test_classifier.py
+++ tests/test_classifier.py
@@ -13,19 +13,23 @@ class DeepHashingFastDevUnitTests(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.project_dir = Path(os.path.dirname(project.__file__))
-        self.dataset_dir = ''
-        self.logs_dir = ''
+        self.dataset_dir = self.project_dir.joinpath('dataset_dir')
+        self.dataset_dir.mkdir(exist_ok=True)
+        self.logs_dir = self.project_dir.joinpath('logs_dir')
+        self.logs_dir.mkdir(exist_ok=True)
 
     def test_sota_2016_CVPR_DSH(self):
         from project.sota_2016_CVPR_DSH import main
         test_file = str(self.project_dir.joinpath('sota_2016_CVPR_DSH.py'))
-        sys.argv = [test_file, '--fast_dev=1', '--accelerator=cpu', '--num_workers=0', '--no-finetune']
+        sys.argv = [test_file, '--fast_dev=1', '--accelerator=cpu', '--num_workers=0', '--no-finetune',
+                    f'--dataset_dir={str(self.dataset_dir)}', f'--logs_dir={str(self.logs_dir)}']
         main()
 
     def test_sota_2017_NIPS_DSDH(self):
         from project.sota_2017_NIPS_DSDH import main
         test_file = str(self.project_dir.joinpath('sota_2017_NIPS_DSDH.py'))
-        sys.argv = [test_file, '--fast_dev=1', '--accelerator=cpu', '--num_workers=0']
+        sys.argv = [test_file, '--fast_dev=1', '--accelerator=cpu', '--num_workers=0',
+                    f'--dataset_dir={str(self.dataset_dir)}', f'--logs_dir={str(self.logs_dir)}']
         main()
 
 
